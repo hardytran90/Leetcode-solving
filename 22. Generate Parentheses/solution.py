353a297648eea054b9a142cb7163d8eecb8ef14b
hardytran90 <vinhhung90@gmail.com>
2026-08-14 23:09:49 +0930

    Complete

--- 22. Generate Parentheses/solution.py
+++ 22. Generate Parentheses/solution.py
@@ -16,3 +16,21 @@ Constraints:
 
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
+        result = []
+        def backtrack(current, open_count, close_count, n, result):
+            if open_count == close_count == n:
+                result.append(current)
+            
+            if open_count < n:
+                backtrack(current + "(", open_count + 1, close_count, n, result)
+                
+            if close_count < open_count:
+                backtrack(current + ")", open_count, close_count + 1, n, result)
+        
+        backtrack("", 0, 0, n, result)
+        return result
+    
+if __name__ == "__main__":
+    solution = Solution()
+    n = 3
+    print(solution.generateParenthesis(n))
