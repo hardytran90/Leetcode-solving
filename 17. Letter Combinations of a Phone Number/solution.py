fec38682d3529e5a2c1a46162907429cbd99b35f
hardytran90 <vinhhung90@gmail.com>
2026-08-15 07:52:23 +0930

    Initial commit

---
+++ 17. Letter Combinations of a Phone Number/solution.py
@@ -0,0 +1,12 @@
+'''
+Given a string containing digits from 2-9 inclusive, 
+return all possible letter combinations that the number could represent. 
+Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons). 
+Note that 1 does not map to any letters.
+'''
+
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        
