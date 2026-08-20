3c953953d4e9e54bb273ded3873fbc62d67bdd44
hardytran90 <vinhhung90@gmail.com>
2026-08-20 23:07:27 +0930

    79. Word Search - Update

--- 79. Word Search [M]/solution.py
+++ 79. Word Search [M]/solution.py
@@ -36,4 +36,44 @@ board and word consists of only lowercase and uppercase English letters.
 '''
 class Solution:
     def exist(self, board: list[list[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
         
+        def backtrack(i, j, k):
+            if k == len(word):
+                return True
+            
+            if k == len(word):
+                return True
+
+            if (i < 0 or i >= m or j < 0 or j >= n 
+                    or board[i][j] != word[k]):
+                return False
+
+            temp = board[i][j]
+            board[i][j] = '#'
+
+            found = (backtrack(i + 1, j, k + 1) or
+                     backtrack(i - 1, j, k + 1) or
+                     backtrack(i, j + 1, k + 1) or
+                     backtrack(i, j - 1, k + 1))
+
+            board[i][j] = temp
+
+            return found
+
+        for i in range(m):
+            for j in range(n):
+                if backtrack(i, j, 0):
+                    return True
+
+        return False
+    
+if __name__ == "__main__":
+    solution = Solution()
+    board = [["A","B","C","E"],
+            ["S","F","C","S"],
+            ["A","D","E","E"]], 
+    word1 = "ABCCED"
+    word2 = "SEE"
+    print(solution.exist(board, word1))
+    print(solution.exist(board, word2))
