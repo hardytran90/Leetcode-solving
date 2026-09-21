70ded9000ae0d5569381709c90eb6ec4318c24cb
hardytran90 <vinhhung90@gmail.com>
2026-09-21 23:07:04 +0930

    Complete

--- Array/561. Array Partition/solution.py
+++ Array/561. Array Partition/solution.py
@@ -27,4 +27,10 @@ nums.length == 2 * n
 
 class Solution:
     def arrayPairSum(self, nums: list[int]) -> int:
-        
+        nums.sort()
+        return sum(nums[::2])
+    
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1,4,3,2]
+    print(solution.arrayPairSum(nums))
